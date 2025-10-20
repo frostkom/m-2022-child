73b4631f3b72c2dba84d34f95a85b8e7a9867a33
frostkom <martin.fors@frostkom.se>
2025-10-20 06:05:47 +0200

    Remove @media all

--- include/style.php
+++ include/style.php
@@ -88,270 +88,267 @@ echo "@font-face
 	font-display: auto
 }
 
-@media all
+body
 {
-	body
+	font-family: HelveticaNeue;
+}
+
+header.full_width > div
+{
+	max-width: none;
+}
+
+	header #site_logo span
 	{
-		font-family: HelveticaNeue;
+		display: none;
 	}
 
-	header.full_width > div
+	header .theme_menu, header > div > #primary_nav
 	{
-		max-width: none;
+		float: left;
+		font-weight: 300;
+		max-width: 82%;
 	}
 
-		header #site_logo span
-		{
-			display: none;
-		}
-
-		header .theme_menu, header > div > #primary_nav
-		{
-			float: left;
-			font-weight: 300;
-			max-width: 82%;
-		}
-
-			header #primary_nav a:hover
-			{
-				text-decoration: underline;
-			}
-
-				header .theme_nav.is_mobile_ready .sub-menu
-				{
-					padding-top: .2em;
-					min-width: 12em;
-				}
-
-					header .theme_nav.is_mobile_ready .sub-menu:before
-					{
-						top: -1em;
-					}
-
-		header .searchform
-		{
-			margin: .6em 0 0 0;
-		}
-
-			.is_mobile header .searchform
-			{
-				margin: .3em 2em 0 0;
-			}
-
-	#mf-after-header
-	{
-		display: none !important;
-	}
-
-	#wrapper #mf-slide-nav
-	{
-		background: none;
-	}
-
-		#mf-slide-nav a:hover
+		header #primary_nav a:hover
 		{
 			text-decoration: underline;
 		}
 
-	#mf-pre-content.full_width > div, #mf-pre-content.full_width > div > .widget > div.has_image
-	{
-		padding: 0;
-		max-width: none !important;
-	}
-
-		.widget.hero .has_text
-		{";
-
-			if(isset($obj_theme_core->options['website_max_width']) && $obj_theme_core->options['website_max_width'] > 0)
+			header .theme_nav.is_mobile_ready .sub-menu
 			{
-				echo "max-width: ".$obj_theme_core->options['website_max_width']."px !important;";
+				padding-top: .2em;
+				min-width: 12em;
 			}
 
-		echo "}";
-
-			if(isset($obj_theme_core->options['heading_size']) && $obj_theme_core->options['heading_size'] > 0)
-			{
-				echo ".widget.hero h3
+				header .theme_nav.is_mobile_ready .sub-menu:before
 				{
-					font-size: ".$obj_theme_core->options['heading_size']." !important;
-				}";
-			}
-
-		echo ".widget.hero .align_right h3, .widget.hero .align_right .content, .widget.hero .align_left h3, .widget.hero .align_left .content
-		{
-			clear: none;
-			float: none;
-			width: 43%;
-		}
-
-			.widget.hero .content_container
-			{
-				color: #213a8f;
-			}
-
-			.widget.hero .align_ontop .image + .content_container
-			{";
-
-				if(isset($obj_theme_core->options['website_max_width']) && $obj_theme_core->options['website_max_width'] > 0)
-				{
-					echo "padding: 0;
-					max-width: ".$obj_theme_core->options['website_max_width']."px !important;";
+					top: -1em;
 				}
 
-			echo "}
-
-				.widget.hero .align_ontop .image + .content_container .content a > p
-				{
-					background-color: #213a8f;
-					border-radius: .4em;
-					color: #93d5f6;
-					display: inline-block;
-					margin-top: 2em;
-					padding: 1.5rem 2rem;
-				}
-
-					.widget.hero .align_ontop .image + .content_container .content a:hover > p
-					{
-						background-color: #f5b5d2;
-						color: #213a8f;
-					}
-
-			.widget.hero .align_right h3, .widget.hero .align_right .content
-			{
-				margin-right: -10% !important;
-			}
-
-			.widget.hero .align_left h3, .widget.hero .align_left .content
-			{
-				margin-left: -10% !important;
-			}
-
-			.widget.hero .align_right .content, .widget.hero .align_left .content
-			{
-				width: 40%;
-			}
-
-				.is_mobile .widget.hero h3, .is_mobile .widget.hero .content
-				{
-					width: 100%;
-				}
-
-		.widget.hero h3 a, .widget.hero h3 span
-		{
-			line-height: 1.3;
-		}
-
-		#mf-content article h1 > a:hover
-		{
-			color: #702283;
-			text-decoration: underline;
-		}
-
-		#mf-content article > .meta > a
-		{
-			color: #702283;
-			font-style: normal;
-			text-decoration: underline;
-		}
-
-			article > .meta > a:before
-			{
-				content: '#';
-			}
-
-		#mf-content ul li a:hover
-		{
-			color: #702283;
-			text-decoration: underline;
-		}
-
-		.widget.login_form #loginform, .widget.lost_password_form .mf_form
-		{
-			background: rgba(147, 213, 246, .4) !important;
-			margin-bottom: 2em;
-		}
-
-		#wrapper #lost_password_link
-		{
-			margin: -1.5em 0 2em 0;
-		}
-
-		/* Typography and sizes on content */
-		/* ########################### */
-		#mf-content .has-small-font-size
-		{
-			font-size: 1.3em;
-		}
-
-		#mf-content .has-medium-font-size
-		{
-			font-size: 2em;
-		}
-
-		#mf-content .has-large-font-size
-		{
-			font-size: 3.6em;
-		}
-		/* ########################### */
-
-		.aside:not(.right) h3
-		{
-			font-size: 1.8em;
-			text-align: center;
-		}
-
-		.widget.social_feed h3
-		{
-			font-weight: normal;
-			padding: .5em 0 1em;
-			text-align: center;
-		}
-
-	footer .vcard .social.circle .fa, footer .vcard .social.circle .fab
+	header .searchform
 	{
-		font-size: 1.5em;
+		margin: .6em 0 0 0;
 	}
 
-	footer .vcard .social.colorize .fa, footer .vcard .social.colorize .fab
-	{
-		background: #213a8f;
-		border-color: #213a8f;
-		color: #93d5f6;
-	}
-
-		footer .vcard .social.colorize .fa:hover, footer .vcard .social.colorize .fab:hover
+		.is_mobile header .searchform
 		{
-			background: #702283;
-			border-color: #702283;
-			color: #93d5f6;
+			margin: .3em 2em 0 0;
 		}
 
-		footer .vcard .social.colorize .fa:after, footer .vcard .social.colorize .fab:after
-		{
-			display: none;
-		}
+#mf-after-header
+{
+	display: none !important;
+}
 
-	footer a:hover
+#wrapper #mf-slide-nav
+{
+	background: none;
+}
+
+	#mf-slide-nav a:hover
 	{
 		text-decoration: underline;
 	}
 
-	footer .widget h3
+#mf-pre-content.full_width > div, #mf-pre-content.full_width > div > .widget > div.has_image
+{
+	padding: 0;
+	max-width: none !important;
+}
+
+	.widget.hero .has_text
+	{";
+
+		if(isset($obj_theme_core->options['website_max_width']) && $obj_theme_core->options['website_max_width'] > 0)
+		{
+			echo "max-width: ".$obj_theme_core->options['website_max_width']."px !important;";
+		}
+
+	echo "}";
+
+		if(isset($obj_theme_core->options['heading_size']) && $obj_theme_core->options['heading_size'] > 0)
+		{
+			echo ".widget.hero h3
+			{
+				font-size: ".$obj_theme_core->options['heading_size']." !important;
+			}";
+		}
+
+	echo ".widget.hero .align_right h3, .widget.hero .align_right .content, .widget.hero .align_left h3, .widget.hero .align_left .content
 	{
-		font-weight: 900;
+		clear: none;
+		float: none;
+		width: 43%;
 	}
 
-	footer .theme_logo
+		.widget.hero .content_container
+		{
+			color: #213a8f;
+		}
+
+		.widget.hero .align_ontop .image + .content_container
+		{";
+
+			if(isset($obj_theme_core->options['website_max_width']) && $obj_theme_core->options['website_max_width'] > 0)
+			{
+				echo "padding: 0;
+				max-width: ".$obj_theme_core->options['website_max_width']."px !important;";
+			}
+
+		echo "}
+
+			.widget.hero .align_ontop .image + .content_container .content a > p
+			{
+				background-color: #213a8f;
+				border-radius: .4em;
+				color: #93d5f6;
+				display: inline-block;
+				margin-top: 2em;
+				padding: 1.5rem 2rem;
+			}
+
+				.widget.hero .align_ontop .image + .content_container .content a:hover > p
+				{
+					background-color: #f5b5d2;
+					color: #213a8f;
+				}
+
+		.widget.hero .align_right h3, .widget.hero .align_right .content
+		{
+			margin-right: -10% !important;
+		}
+
+		.widget.hero .align_left h3, .widget.hero .align_left .content
+		{
+			margin-left: -10% !important;
+		}
+
+		.widget.hero .align_right .content, .widget.hero .align_left .content
+		{
+			width: 40%;
+		}
+
+			.is_mobile .widget.hero h3, .is_mobile .widget.hero .content
+			{
+				width: 100%;
+			}
+
+	.widget.hero h3 a, .widget.hero h3 span
 	{
-		padding-top: 5em;
+		line-height: 1.3;
+	}
+
+	#mf-content article h1 > a:hover
+	{
+		color: #702283;
+		text-decoration: underline;
+	}
+
+	#mf-content article > .meta > a
+	{
+		color: #702283;
+		font-style: normal;
+		text-decoration: underline;
+	}
+
+		article > .meta > a:before
+		{
+			content: '#';
+		}
+
+	#mf-content ul li a:hover
+	{
+		color: #702283;
+		text-decoration: underline;
+	}
+
+	.widget.login_form #loginform, .widget.lost_password_form .mf_form
+	{
+		background: rgba(147, 213, 246, .4) !important;
+		margin-bottom: 2em;
+	}
+
+	#wrapper #lost_password_link
+	{
+		margin: -1.5em 0 2em 0;
+	}
+
+	/* Typography and sizes on content */
+	/* ########################### */
+	#mf-content .has-small-font-size
+	{
+		font-size: 1.3em;
+	}
+
+	#mf-content .has-medium-font-size
+	{
+		font-size: 2em;
+	}
+
+	#mf-content .has-large-font-size
+	{
+		font-size: 3.6em;
+	}
+	/* ########################### */
+
+	.aside:not(.right) h3
+	{
+		font-size: 1.8em;
 		text-align: center;
 	}
 
-	footer .theme_logo #site_logo
+	.widget.social_feed h3
 	{
-		display: block;
-		float: none;
-		margin: 0 auto;
-		width: 30%;
+		font-weight: normal;
+		padding: .5em 0 1em;
+		text-align: center;
 	}
+
+footer .vcard .social.circle .fa, footer .vcard .social.circle .fab
+{
+	font-size: 1.5em;
+}
+
+footer .vcard .social.colorize .fa, footer .vcard .social.colorize .fab
+{
+	background: #213a8f;
+	border-color: #213a8f;
+	color: #93d5f6;
+}
+
+	footer .vcard .social.colorize .fa:hover, footer .vcard .social.colorize .fab:hover
+	{
+		background: #702283;
+		border-color: #702283;
+		color: #93d5f6;
+	}
+
+	footer .vcard .social.colorize .fa:after, footer .vcard .social.colorize .fab:after
+	{
+		display: none;
+	}
+
+footer a:hover
+{
+	text-decoration: underline;
+}
+
+footer .widget h3
+{
+	font-weight: 900;
+}
+
+footer .theme_logo
+{
+	padding-top: 5em;
+	text-align: center;
+}
+
+footer .theme_logo #site_logo
+{
+	display: block;
+	float: none;
+	margin: 0 auto;
+	width: 30%;
 }";
